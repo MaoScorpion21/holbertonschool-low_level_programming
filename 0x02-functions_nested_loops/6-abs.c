d06f7e2136b10729f8692cf04603bc0a27128728
MaoScorpion21 <mao7021@hotmail.com>
2022-02-15 23:53:34 -0500

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - Write a function that computes the absolute value of an integer
+ *
+ *@n: number
+ * Return: 0 succesfully
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
