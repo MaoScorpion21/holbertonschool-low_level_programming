246eb7503bbb27acca046732552bfa7d482735f0
MaoScorpion21 <mao7021@hotmail.com>
2022-03-01 23:51:19 -0500

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory space to be filled
+ * @b: byte to fill with
+ * @n: number of spaces to fill
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+	{
+		*(s + c) = b;
+	}
+	return (s);
+}
